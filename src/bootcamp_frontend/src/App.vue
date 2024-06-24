e55cb993e6d172081678c6747b1c81af476905ef
ninja2137 <t.kunicki@fedora.email>
2024-06-24 19:09:23 +0200

    Dodano licznik oraz wykasowano formularz, który nie wiem co robił

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -1,28 +1,26 @@
-<script setup>
-import { ref } from 'vue';
-import { bootcamp_backend } from 'declarations/bootcamp_backend/index';
-let greeting = ref('');
 
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await bootcamp_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
-}
-</script>
 
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
-      <button type="submit">Click Me!</button>
-    </form>
-    <section id="greeting">{{ greeting }}</section>
+    Siema!
+  <br/>
+  <button @click="podbij"> podbij</button>
+  {{ licznik }}
   </main>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      licznik: 0
+    }
+   
+  },
+  methods: {
+    podbij() {
+      this.licznik++
+    }
+  }
+}
+</script>
